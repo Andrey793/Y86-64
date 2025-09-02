ad4e34e34158e680e8606142696a5cffc5a3daea
andrey <rudakov.andrej1@yandex.ru>
2025-09-03 00:06:19 +0300

    SEQ v1

--- CPU.py
+++ CPU.py
@@ -1,7 +1,7 @@
 from core_blocks import *
 from myhdl._Signal import _Signal, Signal
-from enum import Enum
 from myhdl import always, StopSimulation, delay
+from utils import check_diff, SAOK
 
 
 MEM_SIZE = 1024
@@ -31,6 +31,7 @@ def reset_generator(reset, clk, reset_cycles=3):
 
 @block 
 def CPU(program: list[intbv], clk: _Signal, reset: _Signal, main: int):
+    # Pipeline signals
     pc = Signal(intbv(main)[64:])
     icode = Signal(intbv(0)[4:])
     ifun = Signal(intbv(0)[4:])
@@ -44,84 +45,47 @@ def CPU(program: list[intbv], clk: _Signal, reset: _Signal, main: int):
     valP = Signal(intbv(0)[64:])
     srcA = Signal(intbv(0)[4:])
     srcB = Signal(intbv(0)[4:])
-    Regs = [Signal(intbv(0)[64:].signed()) for _ in range(15)]
-    #rsp
-    Regs[4] = Signal(intbv(MEM_SIZE - 1)[64:])
-    old_Regs = [Signal(Regs[i].val) for i in range(15)]
-
     dstE = Signal(intbv(0)[4:])
     dstM = Signal(intbv(0)[4:])
     Cnd = Signal(intbv(0)[1:])
     Stat = Signal(intbv(0)[3:])
     CC = Signal(intbv(0)[3:])
-    old_CC = Signal(intbv(0)[3:])
     imem_error = Signal(intbv(0)[1:])
     dmem_error = Signal(intbv(0)[1:])
     instr_valid = Signal(intbv(1)[1:])
+    
+    # Register file
+    Regs = [Signal(intbv(0)[64:].signed()) for _ in range(15)]
+    #rsp
+    Regs[4] = Signal(intbv(MEM_SIZE - 1)[64:])
+    old_Regs = [Signal(Regs[i].val) for i in range(15)]
+
+    # Memory
     mem = [Signal(intbv(0)[64:].signed()) for _ in range(MEM_SIZE)]
-    old_mem = [Signal(intbv(0)[64:].signed()) for _ in range(MEM_SIZE)]
+    old_mem = [Signal(mem[i].val) for i in range(MEM_SIZE)]
+    old_CC = Signal(CC.val)
 
-
-    # Stage enables (only one active at a time)
-    en_fetch = Signal(bool(0))
-    en_decode = Signal(bool(0))
-    en_execute = Signal(bool(0))
-    en_memory = Signal(bool(0))
-    en_writeback = Signal(bool(0))
-    en_PC_update = Signal(bool(0))
-
-    fetching_inst = fetching(program, clk, reset, pc, icode, ifun, rA, rB, valC, valP, instr_valid, imem_error, en_fetch)
-    decoding_inst = decoding(clk, reset, icode, rA, rB, valA, valB, srcA, srcB, Regs, en_decode)
-    execution_inst = execution(clk, reset, icode, ifun, valA, valB, valC, valE, Cnd, CC, dstE, dstM, rA, rB, en_execute)
-    memory_access_inst = memory_access(clk, reset, icode, valE, valA, valP, valM, imem_error, dmem_error, instr_valid, Stat, mem, en_memory)
-    writing_back_inst = writing_back(clk, reset, valM, valE, dstE, dstM, Regs, en_writeback, icode)
-    PC_update_inst = PC_update(clk, reset, valP, valM, Cnd, Stat, pc, icode, valC, en_PC_update)
-
-    class t_state(Enum):
-        F = 1
-        D = 2
-        E = 3
-        M = 4
-        W = 5
-        PC = 6
-        HALT = 7
-    state = Signal(t_state.F)
+    # Create the pipeline cycle block
+    pipeline = pipeline_cycle(program, clk, reset, 
+                             pc, icode, ifun, rA, rB, valC, valP, instr_valid, imem_error,
+                             srcA, srcB, valA, valB, valE, Cnd, CC, dstE, dstM,
+                             valM, dmem_error, Stat, Regs, mem)
 
     @always_seq(clk.posedge, reset=reset)
     def controller():
+        # Check for differences and update old values
         check_diff(old_Regs, Regs, old_mem, mem, old_CC, CC)
-        if state.val == t_state.HALT:
-            raise StopSimulation(f"Executions was stopped at time {myhdl.now()}. State:{bin(Stat.val)}")
+        
+        # Check for halt condition
+        if Stat.val != SAOK:
+            raise StopSimulation(f"Execution was stopped at time {myhdl.now()}. State:{bin(Stat.val)}")
 
+        # Update old values for next cycle
         for i in range(15):
             old_Regs[i].next = Regs[i].val
         for i in range(MEM_SIZE):
             old_mem[i].next = mem[i].val
         old_CC.next = CC.val
-        # Set enables based on current state
-        en_fetch.next = (state.val == t_state.F)
-        en_decode.next = (state.val == t_state.D)
-        en_execute.next = (state.val == t_state.E)
-        en_memory.next = (state.val == t_state.M)
-        en_writeback.next = (state.val == t_state.W)
-        en_PC_update.next = (state.val == t_state.PC)
-        
-        # Advance to next state
-        if state.val == t_state.F:
-            state.next = t_state.D
-        elif state.val == t_state.D:
-            state.next = t_state.E
-        elif state.val == t_state.E:
-            state.next = t_state.M
-        elif state.val == t_state.M:
-            state.next = t_state.W
-        elif state.val == t_state.W:
-            state.next = t_state.PC
-        elif state.val == t_state.PC:
-            if Stat.val in [SHLT, SADR, SINS]:
-                state.next = t_state.HALT
-            else:
-                state.next = t_state.F
                 
-    return fetching_inst, decoding_inst, execution_inst, memory_access_inst, writing_back_inst, PC_update_inst, controller
+    return pipeline, controller
 
--- core_blocks.py
+++ core_blocks.py
@@ -4,15 +4,23 @@ from utils import *
 
 
 @block
-def fetching(program: list[intbv], clk: _Signal, reset: _Signal, pc: _Signal, 
-                            icode: _Signal, ifun: _Signal, rA: _Signal, rB: _Signal, 
-                            valC: _Signal, valP: _Signal, instr_valid: _Signal, \
-                            imem_error: _Signal, enable: _Signal):
+def pipeline_cycle(program: list[intbv], clk: _Signal, reset: _Signal, 
+                   # Fetch stage signals
+                   pc: _Signal, icode: _Signal, ifun: _Signal, rA: _Signal, rB: _Signal, 
+                   valC: _Signal, valP: _Signal, instr_valid: _Signal, imem_error: _Signal,
+                   # Decode stage signals  
+                   srcA: _Signal, srcB: _Signal, valA: _Signal, valB: _Signal,
+                   # Execute stage signals
+                   valE: _Signal, Cnd: _Signal, CC: _Signal, dstE: _Signal, dstM: _Signal,
+                   # Memory stage signals
+                   valM: _Signal, dmem_error: _Signal, Stat: _Signal,
+                   # Register file and memory
+                   Regs: list[_Signal], mem: list[intbv]):
+    
     @always_seq(clk.posedge, reset=reset)
-    def fetch():
-        if not enable.val:
-            return
+    def cycle():
         
+        # ===== FETCH STAGE =====
         # Read current PC value
         pc_val = int(pc)
         if pc_val < 0 or pc_val >= len(program):
@@ -32,226 +40,191 @@ def fetching(program: list[intbv], clk: _Signal, reset: _Signal, pc: _Signal,
             imem_error.next = 0
             return
 
-        icode.next = icode_val
-        ifun.next = ifun_val
-        
-        # Determine instruction type using named constants
+        # Determine instruction type and calculate immediate values
         is_simple = icode_val in [IHALT, INOP, IJXX, ICALL, IRET]
+        valC_val = intbv(0)[64:]
+        valP_val = pc_val + 1
+        regs = intbv(0)[8:]
         
         if is_simple:
-            rA.next = RNONE
-            rB.next = RNONE
+            rA_val = RNONE
+            rB_val = RNONE
             
             if icode_val in [IHALT, INOP, IRET]:
-                valC.next = intbv(0)[64:]
-                valP.next = pc_val + 1
+                valC_val = intbv(0)[64:]
+                valP_val = pc_val + 1
             else:
                 # Calculate immediate value for JXX and CALL
                 immediate_val = read_8byte_number(program, pc_val + 1)
-                valC.next = intbv(immediate_val)[64:]
-                valP.next = pc_val + 9
+                valC_val = intbv(immediate_val)[64:]
+                valP_val = pc_val + 9
         else:
             # Handle register instructions
             regs = intbv(program[pc_val + 1])[8:]
-            rA.next = regs[8:4]
-            rB.next = regs[4:]
+            rA_val = regs[8:4]
+            rB_val = regs[4:]
             
             # Use named constants for instruction types
             if icode_val in [IIRMOVQ, IRMMOVQ, IMRMOVQ]:
                 immediate_val = read_8byte_number(program, pc_val + 2)
-                valC.next = immediate_val
-                valP.next = pc_val + 10
+                valC_val = immediate_val
+                valP_val = pc_val + 10
             else:
-                valC.next = intbv(0)[64:]
-                valP.next = pc_val + 2
+                valC_val = intbv(0)[64:]
+                valP_val = pc_val + 2
 
-    return fetch
-
-
-@block
-def decoding(clk: _Signal, reset: _Signal,  icode: _Signal, \
-            rA: _Signal, rB: _Signal,  valA: _Signal, valB: _Signal, \
-                    srcA: _Signal, srcB: _Signal, Regs: list[_Signal], enable: _Signal):
-    @always_seq(clk.posedge, reset=reset)
-    def decode():
-        if not enable.val:
-            return
+        valA_val = intbv(0)[64:]
+        valB_val = intbv(0)[64:]
         
-        if icode.val in [IRRMOVQ, IRMMOVQ, IOPQ, IPUSHQ]:
-            srcA.next = int(rA)
-        elif icode.val in [IPOPQ, IRET]:
-            srcA.next = RRSP
-        else:
-            srcA.next = RNONE
+        # ===== DECODE STAGE =====
+        srcA_val = RNONE
+        srcB_val = RNONE
         
-        if icode.val in [IMRMOVQ, IRMMOVQ, IOPQ, ICMOVXX]:
-            srcB.next = int(rB)
-        elif icode.val in [IPUSHQ, ICALL, IRET, IPOPQ]:
-            srcB.next = RRSP
-        else:
-            srcB.next = RNONE
-
-        if srcA.next != RNONE:
-            valA.next = Regs[srcA.next].val
-        if srcB.next != RNONE:
-            valB.next = Regs[srcB.next].val
+        if icode_val in [IRRMOVQ, IRMMOVQ, IOPQ, IPUSHQ]:
+            srcA_val = int(rA_val)
+        elif icode_val in [IPOPQ, IRET]:
+            srcA_val = int(rA_val)
         
+        if icode_val in [IMRMOVQ, IIRMOVQ, IOPQ, ICMOVXX]:
+            srcB_val = int(rB_val)
+        elif icode_val in [IPUSHQ, ICALL, IRET, IPOPQ]:
+            srcB_val = RRSP
         
-    return decode
+        if srcA_val != RNONE:
+            valA_val = Regs[srcA_val].val
+        if srcB_val != RNONE:
+            valB_val = Regs[srcB_val].val
 
-
-
-@block
-def execution(clk: _Signal, reset: _Signal,  icode: _Signal, ifun: _Signal, \
-            valA: _Signal, valB: _Signal, valC: _Signal, \
-                valE: _Signal, Cnd: _Signal, CC: _Signal, \
-                dstE: _Signal, dstM: _Signal, rA: _Signal, rB: _Signal, enable: _Signal):
-    @always_seq(clk.posedge, reset=reset)
-    def execute():
-        if not enable.val:
-            return
-
+        # ===== EXECUTE STAGE =====
         aluA = intbv(0)[64:]
         aluB = intbv(0)[64:]
-        if icode.val in [IRRMOVQ, IOPQ]:
-            aluA = valA.val
-        if icode.val in [IIRMOVQ, IRMMOVQ, IMRMOVQ]:
-            aluA = valC.val
-        if icode.val in [ICALL, IPUSHQ]:
+        if icode_val in [IRRMOVQ, IOPQ]:
+            aluA = valA_val
+        if icode_val in [IIRMOVQ, IRMMOVQ, IMRMOVQ]:
+            aluA = valC_val
+        if icode_val in [ICALL, IPUSHQ]:
             aluA = -8
-        if icode.val in [IRET, IPOPQ]:
+        if icode_val in [IRET, IPOPQ]:
             aluA = 8
 
-        if icode.val in [IMRMOVQ, IOPQ, IRMMOVQ, IPUSHQ, IPOPQ, ICALL, IRET]:
-            aluB = valB.val
+        if icode_val in [IMRMOVQ, IOPQ, IRMMOVQ, IPUSHQ, IPOPQ, ICALL, IRET]:
+            aluB = valB_val
        
-
         #CC = intvbv(0)[3:] and CC[0] -> ZF, CC[1] -> SF, CC[2] -> OF
-        if icode.val == IOPQ:
-            if ifun.val == 0:
-                valE.next = valA + valB
-            elif ifun.val == 1:
-                valE.next = valA - valB
-            elif ifun.val == 2:
-                valE.next = valA & valB
-            elif ifun.val == 3:
-                valE.next = valA ^ valB
-            CC.next = intbv(0)[3:]
-            if valE.next == 0:
-                CC.next[0] = 1
-            if valE.next < 0:
-                CC.next[1] = 1
-            if is_overflow(valA.val, valB.val, valE.val, ifun.val):
-                CC.next[2] = 1
+        valE_val = intbv(0)[64:]
+        CC_val = intbv(0)[3:]
+        
+        if icode_val == IOPQ:
+            if ifun_val == 0:
+                valE_val = aluA + aluB
+            elif ifun_val == 1:
+                valE_val = aluA - aluB
+            elif ifun_val == 2:
+                valE_val = aluA & aluB
+            elif ifun_val == 3:
+                valE_val = aluA ^ aluB
+            if valE_val == 0:
+                CC_val[0] = 1
+            if valE_val < 0:
+                CC_val[1] = 1
+            if is_overflow(valA_val, valB_val, valE_val, ifun_val):
+                CC_val[2] = 1
         else:
-            valE.next = aluA + aluB
+            valE_val = aluA + aluB
             
-        if icode.val == IJXX or icode.val == ICMOVXX:
-            Cnd.next = Cond(CC.val, ifun.val)
+        Cnd_val = 1
+        if icode_val == IJXX or icode_val == ICMOVXX:
+            Cnd_val = Cond(CC_val, ifun_val)
 
         #dstE
-        if icode.val == ICMOVXX and ifun.val != 0:
-            if Cnd.val:
-                dstE.next = rB.val
-            else:
-                dstE.next = RNONE
-        elif icode.val in [IOPQ, IRRMOVQ, IIRMOVQ, IMRMOVQ]:
-            dstE.next = rB.val
-        elif icode.val in [IPUSHQ, ICALL, IPOPQ, IRET]:
-            dstE.next = RRSP
-        else:
-            dstE.next = RNONE
+        dstE_val = RNONE
+        if icode_val == ICMOVXX and ifun_val != 0:
+            if Cnd_val:
+                dstE_val = srcB_val
+        elif icode_val in [IOPQ, IRRMOVQ, IIRMOVQ, IMRMOVQ]:
+            dstE_val = srcB_val
+        elif icode_val in [IPUSHQ, ICALL, IPOPQ, IRET]:
+            dstE_val = RRSP
         
         #dstM
-        if icode.val in [IPOPQ, IMRMOVQ]:
-            dstM.next = rA.val
-        else:
-            dstM.next = RNONE
-        
-    return execute
+        dstM_val = RNONE
+        if icode_val in [IPOPQ, IMRMOVQ]:
+            dstM_val = srcA_val
 
-
-@block
-def memory_access(clk: _Signal, reset: _Signal, icode: _Signal,
-                  valE: _Signal, valA: _Signal, valP: _Signal, valM: _Signal,
-                  imem_error: _Signal, dmem_error: _Signal,
-                  instr_valid: _Signal, Stat: _Signal, mem: list[intbv], enable: _Signal):
-    @always_seq(clk.posedge, reset=reset)
-    def access():
-        if not enable.val:
-            return
-        
+        # ===== MEMORY STAGE =====
         mem_addr = intbv(0)[64:]
         mem_data = intbv(0)[64:]
         mem_read = 0
         mem_write = 0
 
-        if icode.val in [IRMMOVQ, IPUSHQ, ICALL, IMRMOVQ]:
-            mem_addr = valE.val
-        if icode.val in [IPOPQ, IRET]:
-            mem_addr = valA.val
+        if icode_val in [IRMMOVQ, IPUSHQ, ICALL, IMRMOVQ]:
+            mem_addr = valE_val
+        if icode_val in [IPOPQ, IRET]:
+            mem_addr = valB_val
 
-        mem_read = icode.val in [IMRMOVQ, IPOPQ, IRET]
-        mem_write = icode.val in [IRMMOVQ, IPUSHQ, ICALL]
+        mem_read = icode_val in [IMRMOVQ, IPOPQ, IRET]
+        mem_write = icode_val in [IRMMOVQ, IPUSHQ, ICALL]
         
-        if icode.val in [ICALL]:
-            mem_data = valP.val
-        if icode.val in [IPUSHQ, IRMMOVQ]:
-            mem_data = valA.val
+        if icode_val in [ICALL]:
+            mem_data = valP_val
+        if icode_val in [IPUSHQ, IRMMOVQ]:
+            mem_data = valA_val
 
+        dmem_error_val = 0
         if mem_read or mem_write:
-            dmem_error.next = mem_addr >= len(mem)
+            dmem_error_val = mem_addr >= len(mem)
 
-        if mem_read and not dmem_error.next:
-            valM.next = read_8byte_number_sig(mem, mem_addr)
-        if mem_write and not dmem_error.next:
+        valM_val = intbv(0)[64:]
+        if mem_read and not dmem_error_val:
+            valM_val = read_8byte_number_sig(mem, mem_addr)
+        if mem_write and not dmem_error_val:
             write_8byte_number_sig(mem, mem_addr, mem_data)
 
+        # ===== STATUS DETERMINATION =====
+        Stat_val = SAOK
         if imem_error.val:
-            Stat.next = SADR  # Address error
+            Stat_val = SADR  # Address error
         elif not instr_valid.val:
-            Stat.next = SINS  # Invalid instruction
-        elif dmem_error.val:
-            Stat.next = SADR  # Data memory error
-        elif icode.val == IHALT:
-            Stat.next = SHLT  # Halt
+            Stat_val = SINS  # Invalid instruction
+        elif dmem_error_val:
+            Stat_val = SADR  # Data memory error
+        elif icode_val == IHALT:
+            Stat_val = SHLT  # Halt
+
+        # ===== FINAL ASSIGNMENTS TO SIGNALS =====
+        icode.next = icode_val
+        ifun.next = ifun_val
+        rA.next = regs[8:4]
+        rB.next = regs[4:]
+        valC.next = valC_val
+        valP.next = valP_val
+        srcA.next = srcA_val
+        srcB.next = srcB_val
+        valA.next = valA_val
+        valB.next = valB_val
+        valE.next = valE_val
+        Cnd.next = Cnd_val
+        CC.next = CC_val
+        dstE.next = dstE_val
+        dstM.next = dstM_val
+        valM.next = valM_val
+        dmem_error.next = dmem_error_val
+        Stat.next = Stat_val
+
+        # ===== WRITEBACK STAGE =====
+        if dstE_val != RNONE:
+            Regs[int(dstE_val)].next = valE_val
+        if dstM_val != RNONE:
+            Regs[int(dstM_val)].next = valM_val
+
+        # ===== PC UPDATE =====
+        if icode_val == ICALL:
+            pc.next = valC_val
+        elif icode_val == IRET:
+            pc.next = valM_val
+        elif icode_val == IJXX and Cnd_val:
+            pc.next = valC_val
         else:
-            Stat.next = SAOK  # OK - normal operation
-        
-    return access
+            pc.next = valP_val
 
-@block
-def writing_back(clk: _Signal, reset: _Signal, valM: _Signal, valE: _Signal, \
-    dstE: _Signal, dstM: _Signal, Regs: list[_Signal], enable: _Signal, icode: _Signal):
-
-    @always_seq(clk.posedge, reset=reset)
-    def write_back():   
-        if not enable.val:
-            return
-        
-        if dstE.val != RNONE:
-            Regs[int(dstE.val)].next = valE.val
-        if dstM.val != RNONE:
-            Regs[int(dstM.val)].next = valM.val
-    
-    return write_back
-
-@block
-def PC_update(clk: _Signal, reset: _Signal, valP: _Signal, valM: _Signal, \
-    Cnd: _Signal, Stat: _Signal, pc: _Signal, icode: _Signal,\
-         valC: _Signal, enable: _Signal):
-    @always_seq(clk.posedge, reset=reset)
-    def update():
-        if not enable.val:
-            return
-        
-        if icode.val == ICALL:
-            pc.next = valC.val
-        elif icode.val == IRET:
-            pc.next = valM.val
-        elif icode.val == IJXX and Cnd.val:
-            pc.next = valC.val
-        else:
-            pc.next = valP.val
-       
-    return update
+    return cycle
